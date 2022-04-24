4ad574712b12826f565a46f27f76d942fdc3fa29
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-04-25 01:15:20 +0200

    test gradle issue

--- app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/network/ApiAbstract.kt
+++ app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/network/ApiAbstract.kt
@@ -9,7 +9,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
-@RunWith(JUnit4::class)
 abstract class ApiAbstract<T> {
   lateinit var mockWebServer: MockWebServer
 
