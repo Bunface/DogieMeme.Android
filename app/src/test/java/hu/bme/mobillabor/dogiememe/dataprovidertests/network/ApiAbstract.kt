f97977e39e2bc1530a831a4679571da8449c7668
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-04-25 00:45:05 +0200

    test remove mock webserver

--- app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/network/ApiAbstract.kt
+++ app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/network/ApiAbstract.kt
@@ -1,22 +1,5 @@
-/*
- * Designed and developed by 2020 skydoves (Jaewoong Eum)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package hu.bme.mobillabor.dogiememe.dataprovidertests.network
 
-import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
@@ -27,25 +10,20 @@ import java.io.IOException
 
 @RunWith(JUnit4::class)
 abstract class ApiAbstract<T> {
-  lateinit var mockWebServer: MockWebServer
 
   @Throws(IOException::class)
   @Before
   fun mockServer() {
-    mockWebServer = MockWebServer()
-    mockWebServer.start()
   }
 
   @Throws(IOException::class)
   @After
   fun stopServer() {
-    mockWebServer.shutdown()
   }
 
 
   fun createService(clazz: Class<T>): T {
     return Retrofit.Builder()
-      .baseUrl(mockWebServer.url("/"))
       .addConverterFactory(GsonConverterFactory.create())
       .build()
       .create(clazz)
