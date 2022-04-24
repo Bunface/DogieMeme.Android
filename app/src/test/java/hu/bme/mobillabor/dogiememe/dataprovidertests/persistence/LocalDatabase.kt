4d9fee4ca67c55121b376a12e934fffd97f87138
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-04-25 01:09:29 +0200

    test fix gradle issues

--- app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/persistence/LocalDatabase.kt
+++ app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/persistence/LocalDatabase.kt
@@ -1,25 +1,17 @@
 package hu.bme.mobillabor.dogiememe.dataprovidertests.persistence
 
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import hu.bme.mobillabor.dogiememe.datasource.AppDatabase
 import org.junit.After
 import org.junit.Before
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
 abstract class LocalDatabase {
   lateinit var db: AppDatabase
 
   @Before
   fun initDB() {
-    db = Room.inMemoryDatabaseBuilder(getApplicationContext(), AppDatabase::class.java)
-      .allowMainThreadQueries()
-      .build()
   }
 
   @After
   fun closeDB() {
-    db.close()
   }
 }
