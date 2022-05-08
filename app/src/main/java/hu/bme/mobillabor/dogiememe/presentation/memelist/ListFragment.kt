741adef872bf21d3aa1ab5978c2d72dd29677b28
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-05-08 23:24:44 +0200

    dev list fragment UI

--- app/src/main/java/hu/bme/mobillabor/dogiememe/presentation/memelist/ListFragment.kt
+++ app/src/main/java/hu/bme/mobillabor/dogiememe/presentation/memelist/ListFragment.kt
@@ -1,8 +1,62 @@
 package hu.bme.mobillabor.dogiememe.presentation.memelist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import hu.bitraptors.recyclerview.setupRecyclerView
+import hu.bme.mobillabor.dogiememe.R
+import hu.bme.mobillabor.dogiememe._util.recylerview.setUpHeaderAnim
+import hu.bme.mobillabor.dogiememe.databinding.FragmentListBinding
+import hu.bme.mobillabor.dogiememe.model.Meme
+import hu.bme.mobillabor.dogiememe.presentation.memelist.cells.MemeCell
 
 @AndroidEntryPoint
 class ListFragment: Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private val viewModel:ListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView()
+        refreshOnCreated()
+        setUpHeaderAnim(binding.headerTitle, binding.listHeader, binding.memeRecyclerView)
+        subscribeToFragmentOutputs()
+    }
+
+    private fun refreshOnCreated(){
+        viewModel.fetchMemeList()
+    }
+
+    private fun recyclerView(){
+        this.setupRecyclerView(
+            recyclerView = binding.memeRecyclerView,
+            items = viewModel.memeList,
+            delegates = viewModel.delegates
+        )
+    }
+
+    private fun subscribeToFragmentOutputs(){
+        viewModel.fragmentEvents.observe(viewLifecycleOwner){ output ->
+            when(output){
+                is NavigateToDetail -> {
+                    findNavController().navigate(ListFragmentDirections.toDetailFragment(output.memeId))
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/hu/bme/mobillabor/dogiememe/presentation/memelist/ListViewModel.kt
+++ app/src/main/java/hu/bme/mobillabor/dogiememe/presentation/memelist/ListViewModel.kt
@@ -1,9 +1,53 @@
 package hu.bme.mobillabor.dogiememe.presentation.memelist
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.bitraptors.recyclerview.genericlist.GenericListItem
+import hu.bme.mobillabor.dogiememe.presentation.baseclasses.FragmentOutput
+import hu.bme.mobillabor.dogiememe.presentation.baseclasses.UIAction
+import hu.bme.mobillabor.dogiememe.presentation.memelist.cells.MemeCell
+import hu.bme.mobillabor.dogiememe.presentation.memelist.cells.MemeItemClicked
+import hu.bme.mobillabor.dogiememe.presentation.memelist.repository.ListRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ListViewModel @Inject constructor() : ViewModel() {
+class ListViewModel @Inject constructor(
+    private val listRepository: ListRepository
+) : ViewModel() {
+    private val UIEvents = MutableSharedFlow<UIAction>(0, 10)
+    private val _fragmentEvents = MutableSharedFlow<FragmentOutput>(0, 10)
+    val fragmentEvents = _fragmentEvents.asLiveData()
+
+    val memeList : LiveData<List<GenericListItem>> = listRepository.memeList.map{
+            list -> list.take(20).map{MemeCell(it)}
+    }.asLiveData()
+
+    val delegates = listOf(
+        MemeCell.getDelegate(UIEvents)
+    ).toTypedArray()
+
+    init {
+        viewModelScope.launch {
+            UIEvents.collect { event ->
+                when(event){
+                    is MemeItemClicked -> {
+                        _fragmentEvents.tryEmit(NavigateToDetail(event.id))
+                    }
+                }
+            }
+        }
+    }
+
+    fun fetchMemeList(){
+        listRepository.fetchMemeList()
+    }
 }
+
+class NavigateToDetail(val memeId: String): FragmentOutput
