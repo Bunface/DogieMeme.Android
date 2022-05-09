89e29794c903b9f9cbff39d06cc843daeb4656f5
Réka Fazakas <47921568+Bunface@users.noreply.github.com>
2022-05-09 05:02:48 +0200

    test 5

--- app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/persistence/AppDaoTest.kt
+++ app/src/test/java/hu/bme/mobillabor/dogiememe/dataprovidertests/persistence/AppDaoTest.kt
@@ -31,8 +31,6 @@ class AppDaoTest : LocalDatabase() {
 
     //dao.getMemeList() test
     val testDataPassedThroughDB = appDao.getMemeList()
-    println(testDataPassedThroughDB)
-    println(testData)
     assertThat(testDataPassedThroughDB.toString(), `is`(testData.toString()))
   }
 
